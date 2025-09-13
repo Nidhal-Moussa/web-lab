1c317a41ff41660b9f54eb6630765a296831ba7b
Nidhal Moussa <sinidhal2001@gmail.com>
2025-09-13 16:12:01 +0200

    updated JS

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("JS is connected! 🚀");
+const toggleBtn = document.getElementById("theme-toggle");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+  const currentTheme = body.getAttribute("data-theme");
+  if (currentTheme === "dark") {
+    body.removeAttribute("data-theme");
+    toggleBtn.textContent = "🌙 Dark Mode";
+  } else {
+    body.setAttribute("data-theme", "dark");
+    toggleBtn.textContent = "☀️ Light Mode";
+  }
+});
